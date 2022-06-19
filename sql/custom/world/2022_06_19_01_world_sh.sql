ac9a3c59ab73ef1444cf348c8977fbd2336a5fae
Luzifix <luzifix19@gmail.com>
2022-06-19 13:50:36 +0200

    Core/SQL: Fix npc set emote and npc set event commands

---
+++ sql/custom/world/2022_06_19_01_world_sh.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `command` (`name`, `permission`, `help`) VALUES ('npc set emote', 580, 'Syntax: .npc set emote #emoteId\r\n\r\nSet npc default emote');
+REPLACE INTO `command` (`name`, `permission`, `help`) VALUES ('npc set event', 580, 'Syntax: .npc set event #eventId\r\n\r\nSet npc default event');
