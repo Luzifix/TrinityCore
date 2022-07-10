1aefc1f3eb8bf1822f7a29d4595b54b1c599ee30
Luzifix <luzifix19@gmail.com>
2022-07-10 10:36:35 +0200

    Core/Misc: Enable creation of allied races

---
+++ sql/custom/world/2022_07_10_01_world_sh.sql
@@ -0,0 +1,1 @@
+UPDATE playercreateinfo SET `npe_map` = NULL, `npe_position_x` = NULL, `npe_position_y` = NULL, `npe_position_z` = NULL, `npe_orientation` = NULL, `npe_transport_guid` = NULL;
--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -455,9 +455,7 @@ void WorldSession::HandleCharEnum(CharacterDatabaseQueryHolder const& holder)
         WorldPackets::Character::EnumCharactersResult::RaceUnlock raceUnlock;
         raceUnlock.RaceID = requirement.first;
         raceUnlock.HasExpansion = GetAccountExpansion() >= requirement.second.Expansion;
-        raceUnlock.HasAchievement = requirement.second.AchievementId != 0
-            && (sWorld->getBoolConfig(CONFIG_CHARACTER_CREATING_DISABLE_ALLIED_RACE_ACHIEVEMENT_REQUIREMENT)
-                /* || HasAccountAchievement(requirement.second.AchievementId)*/);
+        raceUnlock.HasAchievement = (requirement.second.AchievementId == 0 || sWorld->getBoolConfig(CONFIG_CHARACTER_CREATING_DISABLE_ALLIED_RACE_ACHIEVEMENT_REQUIREMENT));
         charEnum.RaceUnlockData.push_back(raceUnlock);
     }
 
