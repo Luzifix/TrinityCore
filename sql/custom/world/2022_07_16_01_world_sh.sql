743ccf88ef7792e0ed5cb366f10e76bfbcc8e39e
Luzifix <luzifix19@gmail.com>
2022-07-17 03:53:15 +0200

    Core/SQL: Add housing spells by new created characters into actionbar

---
+++ sql/custom/world/2022_07_16_01_world_sh.sql
@@ -0,0 +1,29 @@
+DELETE FROM `playercreateinfo_action`;
+
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (1, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (2, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (3, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (4, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (5, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (6, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (7, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (8, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (9, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (10, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (11, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (22, 3, 11, 27651, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (24, 3, 11, 27651, 0);
+
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (1, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (2, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (3, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (4, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (5, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (6, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (7, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (8, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (9, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (10, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (11, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (22, 3, 10, 29173, 0);
+REPLACE INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (24, 3, 10, 29173, 0);
