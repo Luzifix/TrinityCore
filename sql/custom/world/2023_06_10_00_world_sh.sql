6c610d0543a3888e62a7d8ae2b8907dc67a467d9
Luzifix <luzifix19@gmail.com>
2023-06-10 13:34:19 +0200

    Core/SQL: Fix typo in trinity_strings

---
+++ sql/custom/world/2023_06_10_00_world_sh.sql
@@ -0,0 +1,1 @@
+REPLACE INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES (40000, 'You get %i copper coins.', NULL, NULL, 'Du erhältst %i Kupfermünzen.', NULL, NULL, NULL, NULL, NULL);
