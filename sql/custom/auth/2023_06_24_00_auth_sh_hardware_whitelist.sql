55bc18209bb89a861d0253cdb471f65d989c0ba3
Luzifix <luzifix19@gmail.com>
2023-10-02 14:54:43 +0200

    Core/SQL: Add hardware whitelist entries

---
+++ sql/custom/auth/2023_06_24_00_auth_sh_hardware_whitelist.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (1, NULL, 'FC41ABAD468585347F1DD5943A7C16C3', NULL, NULL);
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (2805, NULL, 'FC41ABAD468585347F1DD5943A7C16C3', NULL, NULL);
