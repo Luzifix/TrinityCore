3cf4fb30977c399920912fb1040f62296d50ea60
Anna <kemper.anna@outlook.de>
2023-11-05 15:35:42 +0100

    Core/SQL: Fixed Custom Undead NPCs

---
+++ sql/custom/world/2023_10_25_00_world_sh.sql
@@ -0,0 +1,66 @@
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000254, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000253, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000241, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000238, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000220, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000219, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000140, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000078, 10138, 102610);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000004, 10138, 102610);
+
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000254, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000253, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000241, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000238, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000220, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000219, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000140, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000078, 10139, 102614);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000004, 10139, 102614);
+
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000254, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000253, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000241, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000238, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000220, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000219, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000140, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000078, 10140, 102618);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000004, 10140, 102618);
+
+
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000252, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000240, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000239, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000237, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000218, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000217, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000164, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000153, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000152, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000137, 10143, 102656);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000077, 10143, 102656);
+
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000252, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000240, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000239, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000237, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000218, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000217, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000164, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000153, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000152, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000137, 10144, 102660);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000077, 10144, 102660);
+
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000252, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000240, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000239, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000237, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000218, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000217, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000164, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000153, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000152, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000137, 10145, 102664);
+REPLACE INTO `creature_template_outfits_customizations` (`outfitID`, `chrCustomizationOptionID`, `chrCustomizationChoiceID`) VALUES (3000000077, 10145, 102664);
