e36fd6cdc3e6948d0f498e48f3bdbfab5f036dac
Luzifix <luzifix19@gmail.com>
2023-11-10 13:59:57 +0100

    Core/SQL: Add mount gear spells

---
+++ sql/custom/world/2023_11_09_00_world_sh.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `creature_template_spell` VALUES (1050000, 0, 800000, -1);
+REPLACE INTO `creature_template_spell` VALUES (1050000, 1, 800001, -1);
+REPLACE INTO `creature_template_spell` VALUES (1050000, 2, 800002, -1);
