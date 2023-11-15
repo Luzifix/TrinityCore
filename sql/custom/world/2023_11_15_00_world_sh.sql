0d7315310146dc9ab130810ed16537dda46a209c
Luzifix <luzifix19@gmail.com>
2023-11-15 22:58:42 +0100

    Core/SQL: DIsable bugged custom chair's in furniture catalog

--- sql/custom/world/2023_11_15_00_world_sh.sql
+++ sql/custom/world/2023_11_15_00_world_sh.sql
@@ -1,3 +1,4 @@
 UPDATE `furniture_catalog` SET `price` = 5000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003071;
 UPDATE `furniture_catalog` SET `price` = 10000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003072;
 UPDATE `furniture_catalog` SET `price` = 15000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003073;
+UPDATE `furniture_catalog` SET `price` = -1, `updated` = UNIX_TIMESTAMP() WHERE `id` IN (659227, 659225, 659223, 659238, 659232, 659206);
