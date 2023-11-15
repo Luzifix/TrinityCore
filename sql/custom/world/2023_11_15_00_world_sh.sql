48255bd482920f6e8d6507b3677327bb90f83a18
Luzifix <luzifix19@gmail.com>
2023-11-15 22:58:55 +0100

    Core/SQL: Fix creature faction on schattenhain maps

--- sql/custom/world/2023_11_15_00_world_sh.sql
+++ sql/custom/world/2023_11_15_00_world_sh.sql
@@ -2,3 +2,5 @@ UPDATE `furniture_catalog` SET `price` = 5000, `updated` = UNIX_TIMESTAMP() WHER
 UPDATE `furniture_catalog` SET `price` = 10000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003072;
 UPDATE `furniture_catalog` SET `price` = 15000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003073;
 UPDATE `furniture_catalog` SET `price` = -1, `updated` = UNIX_TIMESTAMP() WHERE `id` IN (659227, 659225, 659223, 659238, 659232, 659206);
+
+UPDATE creature_template SET faction = 35 WHERE entry IN (SELECT id FROM creature WHERE `map` IN (5000, 5001, 5002) AND id NOT IN (131989, 87762));
