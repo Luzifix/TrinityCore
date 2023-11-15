f04cae23d41eb7add52db3472b4dc54e98a1074c
Luzifix <luzifix19@gmail.com>
2023-11-15 22:57:58 +0100

    Core/SQL: Add mount fuel station objects to the furniture catalog

---
+++ sql/custom/world/2023_11_15_00_world_sh.sql
@@ -0,0 +1,3 @@
+UPDATE `furniture_catalog` SET `price` = 5000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003071;
+UPDATE `furniture_catalog` SET `price` = 10000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003072;
+UPDATE `furniture_catalog` SET `price` = 15000, `updated` = UNIX_TIMESTAMP() WHERE `id` = 1003073;
