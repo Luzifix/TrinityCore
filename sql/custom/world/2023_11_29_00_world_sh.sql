1fc9b21668c6c0d82590e0a1446496cc4e2d2be7
Luzifix <luzifix19@gmail.com>
2023-11-29 14:43:27 +0100

    Core/SQL: Disable buggy animations for pandaren male and kultiraner female

---
+++ sql/custom/world/2023_11_29_00_world_sh.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (29, 32, 1);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (146, 32, 1);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (148, 32, 1);
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (29, 25, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (146, 25, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (148, 25, 0);
+
