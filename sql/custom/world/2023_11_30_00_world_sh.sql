f65bae4689940a43daf7375b355ace1eb916f1f5
Luzifix <luzifix19@gmail.com>
2023-11-30 23:17:46 +0100

    Core/SQL: Disable buggy animations for troll male/female, bloodelf male, kultiraner male, tauren female

---
+++ sql/custom/world/2023_11_30_00_world_sh.sql
@@ -0,0 +1,17 @@
+DELETE FROM `animations_disabled` WHERE `animation_id`=29 AND `race`=25 AND `gender`=0;
+DELETE FROM `animations_disabled` WHERE `animation_id`=29 AND `race`=32 AND `gender`=1;
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (136, 8, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (138, 8, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (144, 8, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (160, 8, 0);
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (150, 8, 0);
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (75, 8, 1);
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (144, 10, 0);
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (148, 32, 0);
+
+REPLACE INTO `animations_disabled` (`animation_id`, `race`, `gender`) VALUES (148, 6, 0);
+
