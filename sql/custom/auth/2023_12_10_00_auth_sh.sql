6a50edb346bb858043e34fc84a241819590bebbc
Luzifix <luzifix19@gmail.com>
2023-12-10 21:18:03 +0100

    Core/SQL: Add new entries to hardware whitelist

---
+++ sql/custom/auth/2023_12_10_00_auth_sh.sql
@@ -0,0 +1,11 @@
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (1527, NULL, '150FD6CCE6F7C306E8A71EB1FF8616CD', NULL, NULL);
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (1997, NULL, '150FD6CCE6F7C306E8A71EB1FF8616CD', NULL, NULL);
+
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (2212, NULL, '30A6CF1B1E854860EF74930C097DFA60', NULL, NULL);
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (582, NULL, '30A6CF1B1E854860EF74930C097DFA60', NULL, NULL);
+
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (3035, NULL, 'B33CFAAA982771C5E7D842A36CCC6B69', NULL, NULL);
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (3036, NULL, 'B33CFAAA982771C5E7D842A36CCC6B69', NULL, NULL);
+
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (3610, NULL, '33E8641367F4C1453FF72B6620309B98', NULL, NULL);
+REPLACE INTO `battlenet_accounts_hardware_whitelist` (`id`, `macHash`, `gatewayMacHash`, `hardwareHash`, `machineHash`) VALUES (4533, NULL, '33E8641367F4C1453FF72B6620309B98', NULL, NULL);
